e6aa20c948522f39312e8f59364cafa524e1f4a8
ID-4306\HOUSEI4306 <namba@housei-inc.com>
2023-03-07 14:01:33 +0900

    add sumfunc

--- test.go
+++ test.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+func sum(a int) int {
+	a = a + 2
+	return a
+}
+
 func main() {
 
 	fmt.Println("hello_world")
 	fmt.Println("good_morning")
+
+	a := 5
+	fmt.Println("before: a = ", a)
+	fmt.Println("after: a = ", sum(a))
 
 }
