17d84742bfb5d994bf046a5ce89e4f7c3163156b
ID-4306\HOUSEI4306 <namba@housei-inc.com>
2023-03-07 14:20:42 +0900

    add multifunc

--- test.go
+++ test.go
@@ -7,13 +7,20 @@ func sum(a int) int {
 	return a
 }
 
+func multi(a int, b int) int {
+	return a * b
+}
+
 func main() {
 
 	fmt.Println("hello_world")
 	fmt.Println("good_evening")
 
 	a := 5
+	b := 4
 	fmt.Println("before: a = ", a)
 	fmt.Println("after: a = ", sum(a))
+
+	fmt.Println("multi: a * b = ", multi(a, b))
 
 }
